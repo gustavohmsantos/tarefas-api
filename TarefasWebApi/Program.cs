78d60d48a79d1ada1a1a64514f8e04cd90d26212
Gustavo Santos <igumartins.2001@gmail.com>
2024-06-14 16:15:26 -0300

    build: adicionado o escopo entre interface do serviço de usuário e o serviço de usuário

--- TarefasWebApi/Program.cs
+++ TarefasWebApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using TarefasWebApi.Data;
+using TarefasWebApi.Services.Usuario;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
